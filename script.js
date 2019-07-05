1c9dcfa6ca36c1a5305c4a5492e89b27364198a7
geraosio <geraosio@gmail.com>
2019-07-05 04:55:41 +0200

    Add todos styled and dynamically from todosUrl

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// Get content from: https://jsonplaceholder.typicode.com
-
-// Get the tabs objects
-var albumsTab = document.querySelector('.albums-tab');
-var todosTab = document.querySelector('.todos-tab');
-albumsTab
-
-// Get the containers for the albums and todos
-const albumsContainer = document.querySelector('.albums-container');
-const todosContainer = document.querySelector('.todos-container');
+//
+// Constants
+//
 
 // Prepend proxyurl to url to fix Access-Control-Allow-Origin access control check
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
-albumsTab.addEventListener('click', function() {
-  
-  albumsTab.classList.add('active');
-  todosTab.classList.remove('active');
-  
-  todosContainer.style.display = "none";
-  albumsContainer.style.display = "flex";
-  
-  const albumsUrl = 'https://jsonplaceholder.typicode.com/albums';
-  
+// URLs
+const albumsUrl = 'https://jsonplaceholder.typicode.com/albums';
+const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+// Get the tabs objects to add event listeners
+const albumsTab = document.querySelector('.albums-tab');
+const todosTab = document.querySelector('.todos-tab');
+
+// Get the containers for the albums and todos
+// To add childs to them dynamically and to show or hide it's content
+// depending on the tab selected.
+const albumsContainer = document.querySelector('.albums-container');
+const todosContainer = document.querySelector('.todos-container');
+
+
+
+
+
+//
+// Functions
+//
+
+function showAllAlbums(){
   // Show all albums
   function showAlbum(album) {
     var albumItemContainer = document.createElement('div');
@@ -55,51 +61,84 @@ albumsTab.addEventListener('click', function() {
     .catch(function(error) {
       console.log('error', error);
     });
+}
+
+// Show all todos
+function showAllTodos() {
+  
+  function showTodo(todo) {
+    var todoItemContainer = document.createElement('div');
+    var todoItemStatus = document.createElement('span');
+    var todoItemTitle = document.createElement('span');
+    var todoItemUserID = document.createElement('span');
+    var todoItemID = document.createElement('span');
+    todoItemContainer.classList.add('todo-item-container');
+    todoItemStatus.classList.add('todo-item-status');
+    todoItemTitle.classList.add('todo-item-title');
+    todoItemUserID.classList.add('todo-item-userid');
+    todoItemID.classList.add('todo-item-id');
+    
+    if (todo.completed == true) {
+      todoItemStatus.textContent = "🔘";
+    } else {
+      todoItemStatus.textContent = "⚪️";
+    }
+    todoItemTitle.textContent = todo.title;
+    todoItemUserID.textContent = 'From User ' + todo.userId;
+    todoItemID.textContent = todo.id;
+    
+    todoItemContainer.appendChild(todoItemStatus);
+    todoItemContainer.appendChild(todoItemTitle);
+    todoItemContainer.appendChild(todoItemUserID);
+    todoItemContainer.appendChild(todoItemID);
+    todosContainer.appendChild(todoItemContainer);
+  }
+
+  fetch(proxyUrl + todosUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(todos) {
+      todos.forEach(function(todo) {
+        showTodo(todo);
+      })
+    })
+    .catch(function(error) {
+      console.log('error', error);
+    });
+}
+
+
+
+
+
+//
+// Script
+//
+
+// Start by showing all albums
+showAllAlbums();
+
+// Add event listener to the albums tab
+albumsTab.addEventListener('click', function() {
+  
+  albumsTab.classList.add('active');
+  todosTab.classList.remove('active');
+  
+  todosContainer.innerHTML = "";
+  // albumsContainer.style.display = "flex";
+  
+  showAllAlbums();
 });
 
-
-
+// Add event listener to the todos tab
 todosTab.addEventListener('click', function() {
   
   todosTab.classList.add('active');
   albumsTab.classList.remove('active');
   
-  albumsContainer.style.display = "none";
-  todosContainer.style.display = "flex";
+  albumsContainer.innerHTML = "";
+  // todosContainer.style.display = "flex";
   
-  const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
-  
-  // Show all albums
-  // function showAlbum(album) {
-  //   var albumItemContainer = document.createElement('div');
-  //   var albumItemTitle = document.createElement('h3');
-  //   var albumItemUserID = document.createElement('p');
-  //   var albumItemID = document.createElement('span');
-  //   albumItemContainer.classList.add('album-item-container');
-  //   albumItemTitle.classList.add('album-item-title');
-  //   albumItemUserID.classList.add('album-item-userid');
-  //   albumItemID.classList.add('album-item-id');
-  //
-  //   albumItemTitle.textContent = album.title;
-  //   albumItemUserID.textContent = 'User ' + album.userId;
-  //   albumItemID.textContent = album.id;
-  //
-  //   albumItemContainer.appendChild(albumItemTitle);
-  //   albumItemContainer.appendChild(albumItemUserID);
-  //   albumItemContainer.appendChild(albumItemID);
-  //   albumsContainer.appendChild(albumItemContainer);
-  // }
-  //
-  // fetch(proxyUrl + todosUrl)
-  //   .then(function(response) {
-  //     return response.json();
-  //   })
-  //   .then(function(todos) {
-  //     todos.forEach(function(todo) {
-  //       showTodo(todo);
-  //     })
-  //   })
-  //   .catch(function(error) {
-  //     console.log('error', error);
-  //   });
+  showAllTodos();
 });
